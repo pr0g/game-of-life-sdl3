0676ba44257d1e8841e0d3eab1fb74320e4cfef1
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2025-02-16 22:06:39 +0000

    add imgui support

--- main.cpp
+++ main.cpp
@@ -3,12 +3,15 @@
 #include <SDL3/SDL_main.h>
 
 #include "fps.h"
+#include "imgui/imgui_impl_sdl3.h"
+#include "imgui/imgui_impl_sdlrenderer3.h"
 
 #include <cassert>
 #include <memory>
 #include <numeric>
 
 #include <as/as-math-ops.hpp>
+#include <imgui.h>
 #include <minimal-cmake-gol/gol.h>
 
 struct game_of_life_t {
@@ -121,6 +124,12 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv) {
 
   *appstate = game_of_life.release();
 
+  ImGui::CreateContext();
+  ImGui::StyleColorsLight();
+
+  ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+  ImGui_ImplSDLRenderer3_Init(renderer);
+
   return SDL_APP_CONTINUE;
 }
 
@@ -164,9 +173,15 @@ SDL_AppResult SDL_AppIterate(void* appstate) {
 
   const auto game_of_life = static_cast<game_of_life_t*>(appstate);
 
+  ImGui_ImplSDLRenderer3_NewFrame();
+  ImGui_ImplSDL3_NewFrame();
+  ImGui::NewFrame();
+
   SDL_SetRenderDrawColorFloat(
     renderer, 0.5f, 0.5f, 0.5f, SDL_ALPHA_OPAQUE_FLOAT);
   SDL_RenderClear(renderer);
+
+  ImGui::ShowDemoWindow();
 
   const float cell_size = 15.0f;
   const auto board_top_left_corner = as::vec2(
@@ -224,12 +239,17 @@ SDL_AppResult SDL_AppIterate(void* appstate) {
   SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
   SDL_RenderDebugText(renderer, 5, 5, debug_string);
 
+  ImGui::Render();
+  ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
+
   SDL_RenderPresent(renderer);
 
   return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
+  ImGui_ImplSDL3_ProcessEvent(event);
+
   if (event->type == SDL_EVENT_QUIT) {
     return SDL_APP_SUCCESS;
   }
@@ -240,4 +260,8 @@ void SDL_AppQuit(void* appstate, SDL_AppResult result) {
   const auto game_of_life = static_cast<game_of_life_t*>(appstate);
   mc_gol_destroy_board(game_of_life->board_);
   delete game_of_life;
+
+  ImGui_ImplSDLRenderer3_Shutdown();
+  ImGui_ImplSDL3_Shutdown();
+  ImGui::DestroyContext();
 }
